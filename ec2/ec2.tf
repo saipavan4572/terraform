e9d3dcd297046242f86b3cb44c9d3ffbf61d2168
saipavan4572 <saipavan4572@gmail.com>
2025-02-18 18:32:52 +0530

    ami-id changes

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,12 +1,14 @@
 # resource <resource-type> <resource-name>
-resource "aws_instance" "db" {
+resource "aws_instance" "ec2" {
 
-    ami = "ami-090252cbe067a9e58"   # this is the ami id for the devops-practice - RHEL9 AMI.
+    ## ami = "ami-090252cbe067a9e58"   # this is the ami id for the devops-practice - RHEL9 AMI.
+    ami                    = "ami-09c813fb71547fc4f"    # new AMI id during 82s-batch
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-    instance_type = "t3.micro"
+    //instance_type = "t3.micro"
+    instance_type = "t2.micro"      # updating the changes on top of already applied infra using terraform.
 
     tags = {
-        Name = "db"
+        Name = "ec2"
     }
 }
 
